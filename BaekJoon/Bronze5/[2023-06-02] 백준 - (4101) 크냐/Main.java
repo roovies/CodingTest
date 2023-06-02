da780ff90d322ab681f232408baae67d1cee6c8a
이지환 <roovies77@gmail.com>
2023-06-02 16:15:51 +0900

    [2023-06-02] 백준 - (브5) 크냐?

---
+++ BaekJoon/Bronze5/[2023-06-02] 백준 - (4101) 크냐/Main.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        while (true){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            if (a==0 && b==0){
+                break;
+            }
+            if (a>b){
+                bw.write("Yes\n");
+            } else{
+                bw.write("No\n");
+            }
+        }
+        bw.flush();
+        bw.close();
+    }
+}
