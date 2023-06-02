590839e3f3b1b590a99146a3a40dbcacb5507f29
이지환 <roovies77@gmail.com>
2023-06-02 16:15:14 +0900

    [2023-06-02] 백준 - (브5) A+b -2

---
+++ BaekJoon/Bronze5/[2023-06-02] 백준 - (2558) A+b -2/Main.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(String.valueOf(a+b));
+        bw.flush();
+        bw.close();
+    }
+}
